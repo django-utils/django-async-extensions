157d76f0344741433162d3a05cb9025061e45e97
amirreza <amir.rsf1380@gmail.com>
2025-02-11 15:11:52 +0330

    implemented async dates CBVs and related classes

--- django_async_extensions/aviews/generic/__init__.py
+++ django_async_extensions/aviews/generic/__init__.py
@@ -3,6 +3,15 @@ from django_async_extensions.aviews.generic.base import (
     AsyncTemplateView,
     AsyncRedirectView,
 )
+from django_async_extensions.aviews.generic.dates import (
+    AsyncArchiveIndexView,
+    AsyncDateDetailView,
+    AsyncDayArchiveView,
+    AsyncMonthArchiveView,
+    AsyncTodayArchiveView,
+    AsyncWeekArchiveView,
+    AsyncYearArchiveView,
+)
 from django_async_extensions.aviews.generic.detail import AsyncDetailView
 from django_async_extensions.aviews.generic.edit import AsyncFormView
 from django_async_extensions.aviews.generic.list import AsyncListView
